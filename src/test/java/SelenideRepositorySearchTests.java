d6ad78b9fba9c9515e57d37880d1fa7bab2ea5a3
Maxim Nikiforov <rezusjkee@mail.ru>
2023-09-13 18:34:42 +0500

    refactor for 17 Java

--- src/test/java/SelenideRepositorySearchTests.java
+++ src/test/java/SelenideRepositorySearchTests.java
@@ -13,6 +13,8 @@ public class SelenideRepositorySearchTests {
         Configuration.baseUrl = "https:github.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+
     }
 
     @Test
@@ -31,32 +33,37 @@ public class SelenideRepositorySearchTests {
         $(byText("SoftAssertions")).shouldHave(text("SoftAssertions")).click();
         // найти примеры кода для JUnit5
         $(".repository-content ").shouldHave(text("Using JUnit5 extend test class:"));
-        $(".markdown-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
-                "class Tests {\n" +
-                "  @Test\n" +
-                "  void test() {\n" +
-                "    Configuration.assertionMode = SOFT;\n" +
-                "    open(\"page.html\");\n" +
-                "\n" +
-                "    $(\"#first\").should(visible).click();\n" +
-                "    $(\"#second\").should(visible).click();\n" +
-                "  }\n" +
-                "}"));
+        $(".markdown-body").shouldHave(text(
+                """
+                        @ExtendWith({SoftAssertsExtension.class})
+                        class Tests {
+                          @Test
+                          void test() {
+                            Configuration.assertionMode = SOFT;
+                            open("page.html");
+                                                
+                            $("#first").should(visible).click();
+                            $("#second").should(visible).click();
+                          }
+                        }
+                        """));
         // Второй пример
         $(".repository-content ").shouldHave(text("Or register extension inside test class:"));
-        $(".markdown-body").shouldHave(text("class Tests {\n" +
-                "  @RegisterExtension \n" +
-                "  static SoftAssertsExtension softAsserts = new SoftAssertsExtension();\n" +
-                "\n" +
-                "  @Test\n" +
-                "  void test() {\n" +
-                "    Configuration.assertionMode = SOFT;\n" +
-                "    open(\"page.html\");\n" +
-                "\n" +
-                "    $(\"#first\").should(visible).click();\n" +
-                "    $(\"#second\").should(visible).click();\n" +
-                "  }\n" +
-                "}"));
+        $(".markdown-body").shouldHave(text("""
+                class Tests {
+                  @RegisterExtension
+                  static SoftAssertsExtension softAsserts = new SoftAssertsExtension();
+                                
+                  @Test
+                  void test() {
+                    Configuration.assertionMode = SOFT;
+                    open("page.html");
+                                
+                    $("#first").should(visible).click();
+                    $("#second").should(visible).click();
+                  }
+                }
+                """));
 
 
     }
